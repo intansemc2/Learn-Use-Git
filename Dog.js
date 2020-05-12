3053ffac2a0cfbde8c5eb606354e250a545a7fc2
intansemc2 <intansemc2@gmail.com>
2020-05-12 12:29:19 +0700

    Add isDead property, Die menthod

--- Dog.js
+++ Dog.js
@@ -2,8 +2,13 @@ function Dog(id, name) {
 	this.id = id;
 	this.name = name;
 	this.stomach = [];
+	this.isDead = false;
 }
 
 Dog.prototype.Eat = function(cat) {
 	this.stomach.push(cat);
+}
+
+Dog.prototype.Die = function() {
+	this.isDead = true;
 }
