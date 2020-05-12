5bac9f876a843ff299d1fceb9dfaecb55f939e2e
intansemc2 <intansemc2@gmail.com>
2020-05-12 10:53:14 +0700

    Add stomach for Cat. Add new file Dog

--- Cat.js
+++ Cat.js
@@ -2,10 +2,15 @@ function Cat(id, name) {
 	this.id = id;
 	this.name = name;
 	this.isDead = false;
+	this.stomach = [];
 }
 
 Cat.prototype.Die = function() {
 	this.isDead = true;
+}
+
+Cat.prototype.Eat = function(mouse) {
+	this.stomach.push(mouse);
 }
 
 
