82500eea6f2d106d12b0f7673392fe01ec29bdce
intansemc2 <intansemc2@gmail.com>
2020-05-13 11:10:00 +0700

    Add menthod Crawling to Snake

--- Snake.js
+++ Snake.js
@@ -7,4 +7,8 @@ Snake.prototype.Bite = function(animal) {
 	console.log("Snake bite " + animal.name);
 };
 
+Snake.prototype.Crawling = function() {
+	console.log("Snake is Crawling....");
+};
+
 module.exports = Snake;
